e1853c294b1f15a0bf48fd2550c8bb392117e2ef
Massimo Imparato <massimo.imparato@utec.edu.pe>
2020-04-13 21:52:42 -0500

    first commit:

---
+++ cmake-build-debug/CMakeFiles/272texQuotes.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "272texQuotes.exe"
+  "272texQuotes.exe.manifest"
+  "272texQuotes.pdb"
+  "CMakeFiles/272texQuotes.dir/main.cpp.obj"
+  "lib272texQuotes.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/272texQuotes.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
