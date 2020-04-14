0bcc87ef699223e09ef8b86b1cd1d7d5e95fd946
Massimo Imparato <massimo.imparato@utec.edu.pe>
2020-04-13 22:59:14 -0500

    Funciona pero el output no es identico, cambie el input a cin

--- main.cpp
+++ main.cpp
@@ -7,13 +7,21 @@ using namespace std;
 int main() {
     char comillas = '"';
     string texto = "", replace1 = "``", replace2 = "''", line = "";
-    fstream* archivo = new fstream("test.txt", ios_base::in);
+    /*fstream* archivo = new fstream("test.txt", ios_base::in);
     if(archivo) {
         while(getline(*archivo,line)) {
-            texto += line + ' ' + "\n";
+            texto += line;
+            texto += ' ';
+            texto += "\n";
         }
     }
-    archivo->close();
+    archivo->close();*/
+
+    while(getline(cin , line )) {
+        texto += line;
+        texto += ' ';
+        texto += "\n";
+    }
     int count = 1;
 
     for(int i = 0 ; i < texto.length() ; i++) {
