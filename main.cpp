ed251702526392385fd04ee24cda9fcabf7e6258
Massimo Imparato <massimo.imparato@utec.edu.pe>
2020-04-13 22:52:08 -0500

    Problema resuelto pero el output no es identico

--- main.cpp
+++ main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <algorithm>
+#include <fstream>
+using namespace std;
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    char comillas = '"';
+    string texto = "", replace1 = "``", replace2 = "''", line = "";
+    fstream* archivo = new fstream("test.txt", ios_base::in);
+    if(archivo) {
+        while(getline(*archivo,line)) {
+            texto += line + ' ' + "\n";
+        }
+    }
+    archivo->close();
+    int count = 1;
+
+    for(int i = 0 ; i < texto.length() ; i++) {
+        if (texto[i] == comillas) {
+            count % 2 == 0 ? texto.insert(i+1,replace2 ) : texto.insert(i+1, replace1);
+            count++;
+        }
+
+    }
+
+    texto.erase(remove(texto.begin(),texto.end(),'"'), texto.end());
+
+    cout << texto;
+
     return 0;
 }
